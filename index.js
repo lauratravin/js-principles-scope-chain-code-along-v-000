eef563ccd4c5791d0dcfef12612d241e61406a59
Laura Messa Travin <laura.travin@gmail.com>
2019-01-17 21:35:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 /* Your code goes here */
+var customerName = 'bob';
+var bestCustomer;
+
+
+function upperCaseCustomerName(){
+  return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+ return bestCustomer  = 'not bob'; // this is supose no happen.
+}
